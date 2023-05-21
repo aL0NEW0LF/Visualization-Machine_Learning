a7820089d5fc92e7809b88ba37ba67fa2177544a
aL0NEW0LF <mohamedfakhreeddine2019@gmail.com>
2023-05-21 19:22:47 +0100

    RNN Model with manual undersampling and treating data with diff nbr of cols

--- woking_model_with_undersampling.py
+++ woking_model_with_undersampling.py
@@ -16,8 +16,10 @@ file2 = pd.read_excel('D:/PFE/Statistical_Indicators/Statistical_Indicators_Test
 file3 = pd.read_excel('D:/PFE/Statistical_Indicators/Statistical_Indicators_Test3_testing.xlsx', sheet_name=None)
 
 # Load and preprocess the data
-data = []
-labels = []
+data_20col = []
+labels_20col = []
+data_10col = []
+labels_10col = []
 
 # # Iterate over each Excel file
 # for file_path in file_paths:
@@ -35,45 +37,83 @@ labels = []
 #         # # Add the label corresponding to the sheet name
 #         # labels.append(sheet_name['Failure_type'])
 
+sequence = file1['Bearing_1'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
+data_20col.append(sequence)
+# sequence = file1['Bearing_2'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
+# data_20col.append(sequence)
+sequence = file1['Bearing_3'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
+data_20col.append(sequence)
+sequence = file1['Bearing_4'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
+data_20col.append(sequence)
+
 sequence = file2['Bearing_1'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
-data.append(sequence)
+data_10col.append(sequence)
 # sequence = file2['Bearing_2'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
 # data.append(sequence)
 sequence = file2['Bearing_3'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
-data.append(sequence)
-sequence = file3['Bearing_3'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
-data.append(sequence)
-sequence = file3['Bearing_4'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
-data.append(sequence)
+data_10col.append(sequence)
 
-labels = ['Defaut bague externe',
-          'Sans defaut',
-          'Defaut bague externe',
-          'Sans defaut']
+sequence = file3['Bearing_3'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
+data_10col.append(sequence)
+sequence = file3['Bearing_4'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
+data_10col.append(sequence)
+
+labels_20col = ['Sans defaut',
+                'Defaut bague intern',
+                'Defaut billes']
+
+labels_10col = ['Defaut bague externe',
+                'Sans defaut',
+                'Defaut bague externe',
+                'Sans defaut']
 
 # Convert data and labels to numpy arrays
-data = np.array(data)
-labels = np.array(labels)
+data_10col = np.array(data_10col)
+labels_10col = np.array(labels_10col)
 
 # Pad sequences to a fixed length
-padded_data = pad_sequences(data, dtype='float32')
+padded_data_10col = pad_sequences(data_10col, dtype='float32')
 
 # Use the label_encoder to encode the labels we have
-encoded_labels = label_encoder.transform(labels)
+encoded_labels_10col = label_encoder.transform(labels_10col)
+
+# Convert data and labels to numpy arrays
+data_20col = np.array(data_20col)
+labels_20col = np.array(labels_20col)
+
+# Pad sequences to a fixed length
+padded_data_20col = pad_sequences(data_20col, dtype='float32')
+
+# Use the label_encoder to encode the labels we have
+encoded_labels_20col = label_encoder.transform(labels_20col)
 
 # # Split the data into training and testing sets
 # X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
 
 # Build the model
-model = Sequential()
-model.add(LSTM(64, input_shape=(None, 10)))  # Assuming num_features is the number of columns in each sheet
-model.add(Dense(4, activation='softmax'))  # Assuming num_classes is the number of unique labels
+model_10col = Sequential()
+model_10col.add(LSTM(64, input_shape=(None, 10)))  # Assuming num_features is the number of columns in each sheet
+model_10col.add(Dense(4, activation='softmax'))  # Assuming num_classes is the number of unique labels
 
-# Compile the model
-model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Compile the model_10col
+model_10col.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# Train the model
-model.fit(padded_data, encoded_labels, batch_size=32, epochs=10)
+# Train the model_10col
+model_10col.fit(padded_data_10col, encoded_labels_10col, batch_size=32, epochs=10)
 
-# Save the trained model
-model.save('working_model_with_undersampling.h5')
+# Save the trained model_10col
+model_10col.save('working_model_with_undersampling_10col.h5')
+
+# Build the model
+model_20col = Sequential()
+model_20col.add(LSTM(64, input_shape=(None, 20)))  # Assuming num_features is the number of columns in each sheet
+model_20col.add(Dense(4, activation='softmax'))  # Assuming num_classes is the number of unique labels
+
+# Compile the model_20col
+model_20col.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model_20col
+model_20col.fit(padded_data_20col, encoded_labels_20col, batch_size=32, epochs=10)
+
+# Save the trained model_20col
+model_20col.save('working_model_with_undersampling_20col.h5')
