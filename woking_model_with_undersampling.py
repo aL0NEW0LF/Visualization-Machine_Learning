eab8d1e73faa34cf8ecd911d34e1a311d5db44de
aL0NEW0LF <mohamedfakhreeddine2019@gmail.com>
2023-05-26 18:52:42 +0100

    model with SimpleRNN layer, testing the model for different combinations of 'Sans defaut' labeled data (Test 1: 20col / Test 2 + Test 3: 10col)

--- woking_model_with_undersampling.py
+++ woking_model_with_undersampling.py
@@ -1,4 +1,3 @@
-import itertools
 import numpy as np
 import pandas as pd
 from keras.utils import pad_sequences
@@ -17,10 +16,8 @@ file2 = pd.read_excel('D:/PFE/Statistical_Indicators/Statistical_Indicators_Test
 file3 = pd.read_excel('D:/PFE/Statistical_Indicators/Statistical_Indicators_Test3_testing.xlsx', sheet_name=None)
 
 # Load and preprocess the data
-SansDefaut_data_20col = []
 data_20col = []
 labels_20col = []
-SansDefaut_data_10col = []
 data_10col = []
 labels_10col = []
 
@@ -40,47 +37,42 @@ labels_10col = []
 #         # # Add the label corresponding to the sheet name
 #         # labels.append(sheet_name['Failure_type'])
 
+sequence = file1['Bearing_1'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
+data_20col.append(sequence)
+# sequence = file1['Bearing_2'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
+# data_20col.append(sequence)
 sequence = file1['Bearing_3'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
 data_20col.append(sequence)
 sequence = file1['Bearing_4'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
 data_20col.append(sequence)
 
-sequence = file1['Bearing_1'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
-SansDefaut_data_20col.append(sequence)
-sequence = file1['Bearing_2'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
-SansDefaut_data_20col.append(sequence)
-
 sequence = file2['Bearing_1'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
 data_10col.append(sequence)
-sequence = file3['Bearing_3'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
+# sequence = file2['Bearing_2'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
+# data.append(sequence)
+sequence = file2['Bearing_3'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
 data_10col.append(sequence)
 
-sequence = file2['Bearing_2'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
-SansDefaut_data_10col.append(sequence)
-sequence = file2['Bearing_3'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
-SansDefaut_data_10col.append(sequence)
-sequence = file2['Bearing_4'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
-SansDefaut_data_10col.append(sequence)
-sequence = file3['Bearing_1'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
-SansDefaut_data_10col.append(sequence)
-sequence = file3['Bearing_2'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
-SansDefaut_data_10col.append(sequence)
+sequence = file3['Bearing_3'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
+data_10col.append(sequence)
 sequence = file3['Bearing_4'].drop(['File Name', 'Failure_type'], axis=1).to_numpy()
-SansDefaut_data_10col.append(sequence)
+data_10col.append(sequence)
 
 labels_20col = ['Sans defaut',
                 'Defaut bague intern',
                 'Defaut billes']
 
 labels_10col = ['Defaut bague externe',
-                'Defaut bague externe',
                 'Sans defaut',
+                'Defaut bague externe',
                 'Sans defaut']
 
 # Convert data and labels to numpy arrays
-# data_10col = np.array(data_10col)
+data_10col = np.array(data_10col)
 labels_10col = np.array(labels_10col)
-SansDefaut_data_10col = np.array(SansDefaut_data_10col)
+
+# Pad sequences to a fixed length
+padded_data_10col = pad_sequences(data_10col, dtype='float32')
 
 # Use the label_encoder to encode the labels we have
 encoded_labels_10col = label_encoder.transform(labels_10col)
@@ -88,121 +80,42 @@ encoded_labels_10col = label_encoder.transform(labels_10col)
 # Convert data and labels to numpy arrays
 data_20col = np.array(data_20col)
 labels_20col = np.array(labels_20col)
-SansDefaut_data_20col = np.array(SansDefaut_data_20col)
+
+# Pad sequences to a fixed length
+padded_data_20col = pad_sequences(data_20col, dtype='float32')
 
 # Use the label_encoder to encode the labels we have
 encoded_labels_20col = label_encoder.transform(labels_20col)
 
-# Select a subset of 'Sans defaut' data randomly for each combination
-best_accuracy_10col = 0.0
-best_model_10col = None
-
-# Generate all possible combinations of 'Sans defaut' indices
-combinations_10col = list(itertools.combinations(SansDefaut_data_10col, 2))
-
-for combination_10col in combinations_10col:
-    selected_data_10col = data_10col.copy()
-    for i in combination_10col:
-        selected_data_10col.append(i)
-    selected_data_10col = np.array(selected_data_10col)
-
-    # Pad sequences to a fixed length
-    padded_data_10col = pad_sequences(selected_data_10col, dtype='float32')
-
-    # Build the model
-    model_10col = Sequential()
-    model_10col.add(LSTM(64, input_shape=(None, 10), return_sequences=True))
-    model_10col.add(SimpleRNN(64))
-    model_10col.add(Dense(4, activation='softmax'))
-
-    # Compile the model_10col
-    model_10col.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-    # Train the model_10col
-    model_10col.fit(padded_data_10col, encoded_labels_10col, batch_size=32, epochs=10)
-
-    # Evaluate the model_10col
-    _, accuracy_10col = model_10col.evaluate(padded_data_10col, encoded_labels_10col)
-
-    if accuracy_10col > best_accuracy_10col:
-        best_accuracy_10col = accuracy_10col
-        best_model_10col = model_10col
-
-# Save the best model
-best_model_10col.save('best_model_10col.h5')
-
-
-# Select a subset of 'Sans defaut' data randomly for each combination
-best_accuracy_20col = 0.0
-best_model_20col = None
-
-# Generate all possible combinations of 'Sans defaut' indices
-combinations_20col = list(itertools.combinations(SansDefaut_data_20col, 1))
-
-for combination_20col in combinations_20col:
-    selected_data_20col = data_20col.copy()
-    for i in combination_20col:
-        selected_data_20col.append(i)
-    selected_data_20col = np.array(selected_data_20col)
-
-    # Pad sequences to a fixed length
-    padded_data_20col = pad_sequences(selected_data_20col, dtype='float32')
-
-    # Build the model
-    model_20col = Sequential()
-    model_20col.add(LSTM(64, input_shape=(None, 20), return_sequences=True))
-    model_20col.add(SimpleRNN(64))
-    model_20col.add(Dense(4, activation='softmax'))
-
-    # Compile the model_20col
-    model_20col.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-    # Train the model_20col
-    model_20col.fit(padded_data_20col, encoded_labels_20col, batch_size=32, epochs=10)
-
-    # Evaluate the model_20col
-    _, accuracy_20col = model_20col.evaluate(padded_data_20col, encoded_labels_20col)
-
-    if accuracy_20col > best_accuracy_20col:
-        best_accuracy_20col = accuracy_20col
-        best_model_20col = model_20col
-
-# Save the best model
-best_model_20col.save('best_model_20col.h5')
-
-
-
 # # Split the data into training and testing sets
 # X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
 
-# # Build the model
-# model_10col = Sequential()
-# model_10col.add(LSTM(64, input_shape=(None, 10), return_sequences=True))
-# model_10col.add(SimpleRNN(64))
-# model_10col.add(Dense(4, activation='softmax'))  # Assuming num_classes is the number of unique labels
-#
-# # Compile the model_10col
-# model_10col.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-#
-# # Train the model_10col
-# model_10col.fit(padded_data_10col, encoded_labels_10col, batch_size=32, epochs=10)
-#
-# # Save the trained model_10col
-# model_10col.save('working_model_with_undersampling_10col_2.h5')
+# Build the model
+model_10col = Sequential()
+model_10col.add(LSTM(64, input_shape=(None, 10), return_sequences=True))  # Assuming num_features is the number of columns in each sheet
+model_10col.add(SimpleRNN(64))
+model_10col.add(Dense(4, activation='softmax'))  # Assuming num_classes is the number of unique labels
 
-# # Build the model
-# model_20col = Sequential()
-# # model_20col.add(LSTM(64, input_shape=(None, 20)))  # Assuming num_features is the number of columns in each sheet
-# # model_20col.add(SimpleRNN(64, input_shape=(None, 20)))
-# model_20col.add(LSTM(64, input_shape=(None, 20), return_sequences=True))
-# model_20col.add(SimpleRNN(64))
-# model_20col.add(Dense(4, activation='softmax'))  # Assuming num_classes is the number of unique labels
+# Compile the model_10col
+model_10col.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# # Compile the model_20col
-# model_20col.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-#
-# # Train the model_20col
-# model_20col.fit(padded_data_20col, encoded_labels_20col, batch_size=32, epochs=10)
-#
-# # Save the trained model_20col
-# model_20col.save('working_model_with_undersampling_20col_2.h5')
+# Train the model_10col
+model_10col.fit(padded_data_10col, encoded_labels_10col, batch_size=32, epochs=10)
+
+# Save the trained model_10col
+model_10col.save('working_model_with_undersampling_10col.h5')
+
+# Build the model
+model_20col = Sequential()
+model_20col.add(LSTM(64, input_shape=(None, 20), return_sequences=True))  # Assuming num_features is the number of columns in each sheet
+model_20col.add(SimpleRNN(64))
+model_20col.add(Dense(4, activation='softmax'))  # Assuming num_classes is the number of unique labels
+
+# Compile the model_20col
+model_20col.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model_20col
+model_20col.fit(padded_data_20col, encoded_labels_20col, batch_size=32, epochs=10)
+
+# Save the trained model_20col
+model_20col.save('working_model_with_undersampling_20col.h5')
