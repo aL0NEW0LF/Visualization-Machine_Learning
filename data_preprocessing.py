6a158189ad3d999666df750df671434be1f5b25f
aL0NEW0LF <mohamedfakhreeddine2019@gmail.com>
2023-06-14 20:13:23 +0100

    some tuning

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -14,15 +14,23 @@ dfs = pd.read_excel(input_file_path, sheet_name=None)
 # create a new dictionary to store the processed data
 processed_dfs = {}
 
+missing_values = False
+duplicates = False
+outliers_detected = 0
+
 # iterate through each sheet in the dictionary
 for sheet_name, df in dfs.items():
     print(f"Processing sheet: {sheet_name}")
 
-    # handling missing values
-    df.dropna(inplace=True)
+    # check for missing values
+    if df.isna().any().any():
+        missing_values = True
+        df.dropna(inpLace=True)
 
-    # removing duplicate rows
-    df.drop_duplicates(inplace=True)
+    # check for duplicate rows
+    if df.duplicated().any():
+        duplicates = True
+        df.drop_duplicates(inpLace=True)
 
     # removing extreme values (outliers)
     # you can use your own logic to identify and remove extreme values here
@@ -40,27 +48,11 @@ for sheet_name, df in dfs.items():
 
         df = df[(df[col] > lower_bound) & (df[col] < upper_bound)]
 
-
-        # # Count number of rows after processing
-        # num_rows_after = df.shape[0]
-        #
-        # # Calculate number of deleted rows
-        # num_deleted_rows = num_rows_before - num_rows_after
-        #
-        # if num_deleted_rows > 0:
-        #     print(f'{num_deleted_rows} rows deleted in column: {col}')
-
-
-    # do further data preprocessing tasks for each sheet as needed
-    # e.g., data normalization, feature engineering, etc.
-
     # add the processed dataframe to the dictionary of processed dataframes without column labels
     processed_dfs[sheet_name] = df.values
 
     # save the processed sheet to a CSV file
     output_file_path = f"{output_folder_path}/{sheet_name}.csv"
     pd.DataFrame(df).to_csv(output_file_path, index=False, header=False)
-
     print(f"Sheet '{sheet_name}' processed and saved to '{output_file_path}'.")
-
 print("All sheets processed and saved to CSV files.")
